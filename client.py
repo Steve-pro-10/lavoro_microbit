590c9016645d970667167a66f9deb7a86842b92a
Steve <168221553+Steve-pro-10@users.noreply.github.com>
2026-05-07 22:30:48 +0200

    Update client.py

--- client.py
+++ client.py
@@ -34,9 +34,10 @@ class Server():
             self.quorum_conf_is_finished = True
             decimale = int(self.binario, 2)
             self.quorum = decimale
+            self.quorum_conf_is_finished = True
             print(self.quorum)
     def config_quorum(self):
-        display.show("configura")
+        display.show("configura quorum")
         while not self.quorum_conf_is_finished:
             if button_a.is_pressed() and button_b.is_pressed():
                 self.on_button_pressed_ab()
@@ -54,7 +55,7 @@ class Server():
         # parte la votazione
             display.show("premi A per iniziare")
             while not self.is_recieving_votes:
-                if button_a.is_pressed() and button_b.is_pressed():
+                if button_a.was_pressed():
                     self.is_recieving_votes = True
             radio.send("1")
             display.show("voto iniziato")
@@ -84,9 +85,10 @@ class Client():
         self.has_perm_to_vote = False
         
     def wait_for_perm_to_vote(self):
-        message = radio.receive()
-        if message == '1':
-            self.has_perm_to_vote = True
+        while not self.has_perm_to_vote:
+            message = radio.receive()
+            if message == '1':
+                self.has_perm_to_vote = True
     def vote_yes(self):
         if not self.has_voted and self.has_perm_to_vote:
             radio.send("s")# si
@@ -102,7 +104,7 @@ class Client():
     def run(self):
         self.wait_for_perm_to_vote()
         self.vote()
-        radio.off()#esce dalla radio
+
 
 class Main():
     def __init__(self):
